e78a9258780e31a1c651914219b5224f1e958ef9
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-02-04 15:59:18 +0000

    :pencil2: fix_up typo in comparison operator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :slack_handle, presence: true, uniqueness: { case_sensitive: false, scope: :channel_handle }
   validates :channel_handle, presence: true, uniqueness: { case_sensitive: false, scope: :slack_handle }
 
-  scope :for_channel, ->(channel_id) { where('channel_handle == ?', channel_id ) }
+  scope :for_channel, ->(channel_id) { where('channel_handle = ?', channel_id ) }
 end
