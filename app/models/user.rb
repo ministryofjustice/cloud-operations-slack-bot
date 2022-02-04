6ebe5bf6e90cdd5078c97a679d50b8d7f1104b8c
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-02-04 12:26:09 +0000

    :white_check_mark: Update scope for User slack+channel_handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :slack_handle, presence: true, uniqueness: { case_sensitive: false }
-  validates :channel_handle, presence: true, uniqueness: { case_sensitive: false }
+  validates :slack_handle, presence: true, uniqueness: { case_sensitive: false, scope: :channel_handle }
+  validates :channel_handle, presence: true, uniqueness: { case_sensitive: false, scope: :slack_handle }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,13 +15,21 @@ RSpec.describe User, :type => :model do
     expect(User.count).to eq 1
   end
 
-  it "does not allow creation when slack_handle already exists" do
-    user1 = User.create(slack_handle: "U1")
-    user2 = User.create(slack_handle: "U1")
+  it "does not allow creation when slack_handle already exists in the same channel" do
+    user1 = User.create(slack_handle: "U1", channel_handle: "C1")
+    user2 = User.create(slack_handle: "U1", channel_handle: "C1")
     expect(user1).to be_valid
     expect(user2).to be_invalid
     expect(User.count).to eq 1
     expect(user2.errors.full_messages.first).to eq "your slack handle has already been registered"
+  end
+  
+  it "does allow creation when slack_handle does not exist in the channel, but exists in another channel" do
+    user1 = User.create(slack_handle: "U1", channel_handle: "C1")
+    user2 = User.create(slack_handle: "U1", channel_handle: "C2")
+    expect(user1).to be_valid
+    expect(user2).to be_valid
+    expect(User.count).to eq 2
   end
 
   it "is null after one user was created in a previous example" do
